750cce951fa87e506fcd69b1259bbd82b15523b7
Niclas Nilsson <thenajnth@gmail.com>
2024-04-13 22:39:48 +0200

    Created actually working program

--- src/update_save_ip_api.py
+++ src/update_save_ip_api.py
@@ -1,3 +1,50 @@
 #!/usr/bin/env python3
 
-print('Hello world!')
+import time
+import os
+import sys
+import requests
+import schedule
+
+KEY = os.getenv('SAVEIPAPI_KEY')
+HOSTNAME = os.getenv('SAVEIPAPI_HOSTNAME')
+APIKEY = os.environ.get('SAVEIPAPI_APIKEY')
+
+
+if KEY == None:
+    print('Unable to find environment variable for SAVEIPAPI_KEY. Program ends.')
+    sys.exit(1)
+if HOSTNAME == None:
+    print('Unable to find environment variable for SAVEIPAPI_HOSTNAME. Program ends.')
+    sys.exit(1)
+if APIKEY == None:
+    print('Unable to find environment variable for SAVEIPAPI_APIKEY. Program ends.')
+    sys.exit(1)
+
+def job():
+    try:
+        response = requests.get('https://api.ipify.org', timeout=10)
+        ip = response.content.decode('ascii')
+
+        post_headers = { 'ApiKey': APIKEY }
+        post_content = { 'ip': ip }
+
+        response = requests.post(
+            f'https://{HOSTNAME}/ip/{KEY}',
+            headers=post_headers,
+            json=post_content,
+            timeout=10)
+        if response.status_code == 200:
+            print(f'Sent ip {ip} to {HOSTNAME}')
+        else:
+            print(f'Unable to send ip. Server replied with status {response.status_code}.')
+    except Exception as e:
+        print (f'Error: Unable to get or send ip: {e}')
+
+schedule.every(1).hours.do(job)
+
+if __name__ == '__main__':
+    print('Starting Application.')
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
